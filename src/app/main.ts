b75a3d310ead2caedf46645a93cc67c1e477cfc8
SGierszewski <stefaniegierszewski@hotmail.com>
2021-05-10 16:18:09 +0200

    :pencil2: Fix typo in main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@ import "./style.css";
 const app = document.querySelector("#app");
 
 if (app !== null) {
-  app.innerHTML = `applicationCache.
+  app.innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
