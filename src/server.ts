71d8554c73c874ec25f43f68363d534d13ea8992
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-07 10:10:48 +0200

    :sparkles: Serve app production bundle

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,8 @@
 import fs from "fs";
+import express from "express";
+
+const app = express();
+
+// Serve app production bundle
+app.use(express.static("dist/app"));
 console.log(fs.readFileSync("./README.md", "utf-8"));
