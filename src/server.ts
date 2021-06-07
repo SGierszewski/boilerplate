9ddd730d71f368d30c723858dcba800964eb342f
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-07 10:18:55 +0200

    :construction_worker: Serve storybook

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,9 @@ import express from "express";
 
 const app = express();
 
+// Serve storybook production bundle
+app.use("/storybook", express.static("dist/storybook"));
+
 // Serve app production bundle
 app.use(express.static("dist/app"));
 console.log(fs.readFileSync("./README.md", "utf-8"));
