887769df536318479f99e0034a5fdccb95196c5a
SGierszewski <stefaniegierszewski@hotmail.com>
2021-06-09 11:26:28 +0200

    Add proxy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const BACKEND_PORT = process.env.PORT;
 
 //https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  server: {
+    proxy: {
+      "/api": {
+        target: `http://localhost:${BACKEND_PORT}`,
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: "dist/app",
   },
